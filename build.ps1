b83d83901da88b0c08ff096dbb977b122951418f
ainerpony <desktopminer@outlook.com>
2026-06-27 08:31:36 +0800

    use dev build as default

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 # For Release build
-cmake -B build_win;
+# cmake -B build_win;
 
 # For dev build
-# cmake -B build_win -DEXEC_OUTPUT_DIR="../src";
+cmake -B build_win -DEXEC_OUTPUT_DIR="../src";
 
 if ($LASTEXITCODE -eq 0) {
-    cmake --build build_win --config Release -j8;
+    cmake --build build_win --config Debug -j8;
 }
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # Release build
-cmake -B build_linux -D CMAKE_BUILD_TYPE=Release &&
+# cmake -B build_linux -D CMAKE_BUILD_TYPE=Release &&
 
 # Dev build
-# cmake -B build_linux -D CMAKE_BUILD_TYPE=Debug -D EXEC_OUTPUT_DIR="../src" &&
+cmake -B build_linux -D CMAKE_BUILD_TYPE=Debug -D EXEC_OUTPUT_DIR="../src" &&
 
 cmake --build build_linux -j8
